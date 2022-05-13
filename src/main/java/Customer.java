45bb7f749e3c3b19df5588d7cb1d9b02ed4d1130
aigenseer <viktor.aigenseer@web.de>
2022-05-13 08:35:17 +0200

    Writing tests

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,7 +2,7 @@
 import java.lang.*;
 import java.util.*;
 
-class Customer {
+public class Customer {
     private String name;
     private Vector rentals = new Vector();
     public Customer (String newname){
